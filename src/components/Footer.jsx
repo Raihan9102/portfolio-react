6e4d40a0d9e5d00e6af015fed1684105f58df7a7
Raihan Thaffan <raihan.rahmat2019@gmail.com>
2025-07-14 16:40:38 +0700

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className="mt-32  md:text-3xl lg:text-3xl py-4 flex justify-between items-center font-bold lg:px-10">
+    <div className="mt-0  md:text-3xl lg:text-3xl py-4 flex justify-between items-center font-bold lg:px-10">
       <h1>Portfolio</h1>
       <div className="px-10 space-x-4 opacity-90 md:px-18 md:space-x-6 lg:space-x-10 lg:px-70">
         <a href="#beranda" className="hover:text-[#696969]">
