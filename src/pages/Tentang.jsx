cf2dec0757a1e0125cd1d01354ce7c02cb91e83d
Raihan Thaffan <raihan.rahmat2019@gmail.com>
2025-08-27 11:20:33 +0700

    update

--- src/pages/Tentang.jsx
+++ src/pages/Tentang.jsx
@@ -11,17 +11,17 @@ export default function Tentang() {
           className="lg:w-150 w-80  rounded-xl  mb-10 mx-auto md:w-150 lg:mx-auto  border border-[#ffff] rounded-3xl "
         />
         <div className=" bg-gradient-to-b from-blue to-black border border-[#FFFF] rounded-2xl mb-10 p-2 ">
-          <h1 className=" text-2xl lg:text-[32px] md:text-[39px]  mb-2 text-center md:text-left ">
+          <h1 className=" text-2xl lg:text-[32px] md:text-[39px]  mb-2 text-center md:text-left lg:ml-20 font-semibold">
             Muhammad Raihan Thaffan Hidayat
           </h1>
-          <ul className="list-disc pl-10  text-[18px] lg:text-[28px] md:text-[30px] mb-2 lg:mb-10">
+          <ul className="list-disc pl-10  text-[18px] lg:text-[28px] md:text-[30px] mb-2 lg:mb-2">
             <li>Kampus : Telkom University</li>
             <li>Jurusan : Teknik Telekomunikasi</li>
             <li>Usia : 20 Tahun</li>
             <li>Minat : IoT, Web Dev, (Embedded System)</li>
             <li>Tools (Basic) : React, Arduino, ESP32, Excel, Mysql</li>
           </ul>
-          <p className="text-[15px] lg:text-[26px] md:text-[28px] items-center italic mb-1 text-center md:text-left ">
+          <p className="text-[15px] lg:text-[26px] md:text-[28px] items-center italic mb-1 lg:mb-10 text-center md:text-left ">
             "Hard work never betrays the result. If the result isn't right,
             maybe it's the way you worked that needs to change."
           </p>
